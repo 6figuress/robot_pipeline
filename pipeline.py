da59722b5819843efa956d1a3b37bd0ef317c74a
MartTave <martin.tavernier256@gmail.com>
2025-03-27 15:33:13 +0100

    wip

--- pipeline.py
+++ pipeline.py
@@ -7,32 +7,33 @@ from ur_ikfast.ur_kinematics import URKinematics, MultiURKinematics
 from calibrate import loadCalibration
 from camera_sync import Transform, vizPoses
 
-mesh = load_mesh("./painting_models/cube/cube_8mm.obj")
+# mesh = load_mesh("./painting_models/cube/cube_8mm.obj")
 
-res = mesh_to_paths(mesh, n_samples=50000)
+# res = mesh_to_paths(mesh, n_samples=50000, verbose=True)
 
-traj = []
 
-traj_transf = []
+# traj = []
+
+# traj_transf = []
 
 # For now, taking only white trajectory
-for r in res:
-    if r[0] == (255, 255, 255, 255):
-        for pose in r[1]:
-            traj_transf.append(
-                Transform.fromQuaternion(
-                    quat=pose[1], tvec=np.array(pose[0]) * 1000, scalar_first=False
-                )
-            )
+# for r in res:
+#     if r[0] == (255, 255, 255, 255):
+#         for pose in r[1]:
+#             traj_transf.append(
+#                 Transform.fromQuaternion(
+#                     quat=pose[1], tvec=np.array(pose[0]) * 1000, scalar_first=False
+#                 )
+#             )
 
 
-# traj_transf: list[Transform] = [
-#     Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[-20.0, 0.0, 200.0]),
-#     Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[-10.0, 0.0, 200.0]),
-#     Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[0.0, 0.0, 200.0]),
-#     Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[10.0, 0.0, 200.0]),
-#     Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[20.0, 0.0, 200.0]),
-# ]
+traj_transf: list[Transform] = [
+    Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[-20.0, 0.0, 400 + 80.0]),
+    Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[-10.0, 0.0, 400 + 80.0]),
+    Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[0.0, 0.0, 400 + 80.0]),
+    Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[10.0, 0.0, 400 + 80.0]),
+    Transform.fromQuaternion(quat=[0.0, 1.0, 0.0, 0.0], tvec=[20.0, 0.0, 400 + 80.0]),
+]
 
 # vizPoses(traj_transf, limits=(-50, 50), length=5)
 
@@ -44,13 +45,13 @@ base2world, grip2cam = loadCalibration(
 
 # Those value are written in the technical plan of the duck support
 duck2world: Transform = Transform.fromRodrigues(
-    rvec=[0.0, 0.0, 0.0], tvec=[117.57, -152.57, 111.0]
+    rvec=[0.0, 0.0, 0.0], tvec=[112.57, -147.57, 111.0]
 )
 
 # TODO: Remove this line !!!!
-duck2world = Transform.fromRodrigues(
-    rvec=[0.0, 0.0, 0.0], tvec=[117.57, -152.57, 250.0]
-)
+# duck2world = Transform.fromRodrigues(
+#     rvec=[0.0, 0.0, 0.0], tvec=[117.57, -152.57, 250.0]
+# )
 
 duck2robot = duck2world.combine(base2world.invert)
 
@@ -63,10 +64,14 @@ for i in range(len(traj_transf)):
 
 # vizPoses(transformed)
 
-kine = URKinematics("ur3e")
+kine = URKinematics("ur3e_with_pen_final")
 
 multi = MultiURKinematics(kine)
 
+
+import ipdb
+
+ipdb.set_trace()
 
 angles = multi.inverse_optimal([t.kine_pose for t in transformed])
 
